af628d8ce187bde56eee228ee864a8a73b03830f
tdy2012 <tdy2020@gmail.com>
2019-08-20 22:29:00 +0700

    [Master] Implement assignment 1

--- run.py
+++ run.py
@@ -23,24 +23,50 @@ START_POINT = (400, 200)
 # Map file
 MAP_FILE = 'maps/default_map.kv'
 
+STEP_SIZE = 5
+COLLISION_AVOID_ANGLE = 50
+COLLISION_DISTANCE_EPSILON = 25
+
 class MyRobot(Robot):
     def __init__(self):
         super(MyRobot, self).__init__()
         self.pos = START_POINT
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
+    
+    def getTurnToFoodAngle(self):
+        ''' Compute angle to turn to food
+        '''
+
+        #   Get minimum distance from all sensors
+        minDistance = self.getMinDistance()
+
+        #   Get smell angle
+        smellAngle = self.smell()
+
+        #   Compute angle ratio to determine if the robot should avoid the obstable
+        #   or move toward the food
+        angleRatio = max(min(minDistance/COLLISION_DISTANCE_EPSILON,1.0),0.0)
+
+        #   Compute the angle with angle ratio
+        turnAngle = (1-angleRatio)*COLLISION_AVOID_ANGLE + angleRatio*smellAngle
+
+        return turnAngle
+
+    def getMinDistance(self):
+        ''' Get minimum distance from all sensors
+        '''
+        allDistanceList = self.distance()
+        return min(allDistanceList)
 
     def update(self):
         ''' Update method which will be called each frame
         '''
-        r = random.randint(0, 3)
-        self.move(5)
-        if(r == 1):
-            self.turn(15)
-        elif(r == 2):
-            self.turn(-15)
-        pass
-    
+
+        angle = self.getTurnToFoodAngle()
+        self.turn(angle)
+        self.move(STEP_SIZE)
+
     def _keyboard_closed(self):
         self._keyboard.unbind(on_key_down=self._on_keyboard_down)
         self._keyboard = None
@@ -54,6 +80,15 @@ class MyRobot(Robot):
             self.turn(5)
         elif keycode[1] == 's':
             self.move(-5)
+        elif keycode[1] == 'r':
+            self.randomStart()    
+
+    def randomStart(self):
+        # MAP SIZE 790, 490
+        OFFSET = 10
+        START_POINT = ( random.randint( OFFSET, 790-OFFSET ),
+                        random.randint( OFFSET, 490-OFFSET ) )
+        self.pos = START_POINT    
 
 if __name__ == '__main__':
     app = PySimbotApp(MyRobot, ROBOT_NUM, mapPath=MAP_FILE, interval=TIME_INTERVAL, maxtick=MAX_TICK)
